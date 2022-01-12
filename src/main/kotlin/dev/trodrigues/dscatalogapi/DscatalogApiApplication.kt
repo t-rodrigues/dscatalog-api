b91ea78ac653e2d4d541ad549cb0559b0d530fe9
Thiago Rodrigues <thiagor_@live.com>
2022-01-12 00:27:02 -0300

    chore: initial commit

---
+++ src/main/kotlin/dev/trodrigues/dscatalogapi/DscatalogApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.trodrigues.dscatalogapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DscatalogApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DscatalogApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/trodrigues/dscatalogapi/DscatalogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.trodrigues.dscatalogapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DscatalogApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
