cccfffcf67acab6e79d3eba20ba4909ed02e6f26
Thiago Rodrigues <thiagor_@live.com>
2022-01-12 01:25:32 -0300

    feat: add Category

---
+++ src/main/kotlin/dev/trodrigues/dscatalogapi/domain/Category.kt
@@ -0,0 +1,17 @@
+package dev.trodrigues.dscatalogapi.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity(name = "tb_category")
+data class Category(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val name: String
+
+)
---
+++ src/main/resources/db/migration/V1__create_tb_category.sql
@@ -0,0 +1,6 @@
+create table tb_category(
+    id   bigint auto_increment,
+    name varchar(100),
+
+    constraint category_pkey primary key (id)
+);
